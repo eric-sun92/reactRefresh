baababdf02aa8369c16a6e5c6805c6890015c37f
Eric Sun <eric.sun.ecs92@yale.edu>
2023-02-11 22:28:45 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Planet from "./Planet";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  const addTaskToList = () => {
+    setToDoList((previous) => {
+      return [
+        ...previous,
+        {
+          id: previous.length == 0 ? 1 : previous[previous.length - 1],
+          name: task,
+        },
+      ];
+    });
+    setTask((previous) => "");
+  };
+
+  const deleteToDo = (toDoid) => {
+    const removedTaskList = toDoList.filter((toDo) => {
+      return toDo.id != toDoid;
+    });
+    setToDoList((previous) => {
+      return removedTaskList;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input value={task} onChange={(e) => setTask(e.target.value)} />
+        <button onClick={addTaskToList}>Add Task</button>
+      </div>
+      <div>
+        {toDoList.map((toDo, key) => {
+          return (
+            <div key={key} style={{ display: "flex" }}>
+              <button key={key} onClick={() => deleteToDo(toDo.id)}>
+                Delete
+              </button>
+              <h1>{toDo.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
+//   const [count, setCount] = useState(0);
+
+//   const decreaseCount = () => {
+//     setCount((previous) => {
+//       return previous - 1;
+//     });
+//   };
+
+//   const increaseCount = () => {
+//     setCount((previous) => {
+//       return previous + 1;
+//     });
+//   };
+
+//   const resetCount = () => {
+//     setCount((previous) => {
+//       return 0;
+//     });
+//   };
+
+//   return (
+//     <div className="App">
+//       <div>
+//         <button onClick={decreaseCount}>Decrease</button>
+//         {count}
+//         <button onClick={increaseCount}>Increase</button>
+//       </div>
+
+//       <button onClick={resetCount}>Reset</button>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Planet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Planet = (props) => {
+  return (
+    <div>
+      <h1>{props.planet.name}</h1>
+    </div>
+  );
+};
+
+export default Planet;
